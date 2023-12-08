f47b62e9d569ba8251fdc1df161d0cf8959a71cc
nandini965 <nandhini965@gmail.com>
2023-12-08 21:45:43 +0530

    filebeat

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,37 +1,37 @@
 
-def call() {
-  pipeline {
-      agent {
-          node {
-              lable 'workstation'
-             }
-         }
-     }
-     options {
-         ansicolor('xterm')
-
-     }
-     stage ('code quality test') {
-         step {
-             sh run code quality test
-         }
-     }
-
-    stage ('checkmark sast') {
-        step {
-            sh
-        }
-    }
-     stage ('checkmark sca') {
-         step {
-
-         }
-     }
-
-     post {
-         always {
-         clear ws ()
-     }
-    }
-     }
-
+//def call() {
+//  pipeline {
+//      agent {
+//          node {
+//              lable 'workstation'
+//             }
+//         }
+//     }
+//     options {
+//         ansicolor('xterm')
+//
+//     }
+//     stage ('code quality test') {
+//         step {
+//             sh run code quality test
+//         }
+//     }
+//
+//    stage ('checkmark sast') {
+//        step {
+//            sh
+//        }
+//    }
+//     stage ('checkmark sca') {
+//         step {
+//
+//         }
+//     }
+//
+//     post {
+//         always {
+//         clear ws ()
+//     }
+//    }
+//     }
+//
