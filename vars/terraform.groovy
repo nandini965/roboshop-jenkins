cb5db33f9ac986a8b9e10b0f1674afa3e0e5a779
nandini965 <nandhini965@gmail.com>
2023-12-09 16:24:06 +0530

    filebeat

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,30 +1,33 @@
-//pipeline {
-//    agent {
-//        node {
-//            label 'workstation'
-//        }
-//    }
-//
-//    parameters {
-//        choice(name: 'env', choices('dev','prod') description: 'pick environment' )
-//    }
-//
-//    options {
-//        ansicolor(xterm)
-//    }
-//
-//    stage ('terraform-init') {
-//        step {
-//            sh 'terraform-init backend_config=env-dev state.tfvars'
-//        }
-//    }
-//    stage ('terraform-apply') {
-//        step {
-//            sh 'terraform-apply -auto-approve=env-dev main.tfvars'
-//        }
-//    }
-//    post {
-//        always {
-//            clear ws()
-//        }
-//    }
+def call () {
+pipeline {
+    agent {
+        node {
+            label 'workstation'
+        }
+    }
+}
+
+    parameters {
+        choice(name: 'env', choices('dev', 'prod') description: 'pick environment')
+    }
+
+    options {
+        ansicolor(xterm)
+    }
+
+    stage('terraform-init') {
+        step {
+            sh 'terraform-init backend_config=env-dev state.tfvars'
+        }
+    }
+    stage('terraform-apply') {
+        step {
+            sh 'terraform-apply -auto-approve=env-dev main.tfvars'
+        }
+    }
+    post {
+        always {
+            clear ws()
+        }
+    }
+}
