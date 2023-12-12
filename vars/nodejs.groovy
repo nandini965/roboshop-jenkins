faa06ff3add09206e1f2f132b4b20314dc7b4ae8
nandini965 <nandhini965@gmail.com>
2023-12-13 01:43:00 +0530

    filebeat

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,43 +1,43 @@
 
 def call() {
-  pipeline {
-      agent {
-          node {
-              lable 'workstation'
-             }
-         }
-     }
-     options {
-         ansicolor('xterm')
+    pipeline {
+        agent {
+            node {
+                lable 'workstation'
+            }
+        }
 
-     }
-     stage ('code quality test') {
-         step {
-             sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host_url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+        options {
+            ansicolor('xterm')
 
-            // sh run code quality test
-         }
-     }
-   stage ('unit testing') {
-       step {
-           sh 'run unit testing'
-       }
-   }
-    stage ('checkmark sast') {
-        step {
-            sh 'run checkmark sast'
+        }
+        stage('code quality test') {
+            step {
+                sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host_url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+
+                // sh run code quality test
+            }
+        }
+        stage('unit testing') {
+            step {
+                sh 'echo unit test'
+            }
+        }
+        stage('checkmark sast') {
+            step {
+                sh 'echo checkmark sast scan'
+            }
+        }
+        stage('checkmark sca scan') {
+            step {
+                sh 'echo checkmark sca scan'
+            }
+        }
+
+        post {
+            always {
+                clear ws()
+            }
         }
     }
-     stage ('checkmark sca') {
-         step {
-        sh 'run checkmark sca'
-         }
-     }
-
-     post {
-         always {
-         clear ws ()
-     }
-    }
-     }
-
+}
