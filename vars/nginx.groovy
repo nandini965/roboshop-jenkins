cae34d0efb20882c0fbe79260d049442406fb0de
nandini965 <nandhini965@gmail.com>
2023-12-13 17:30:00 +0530

    filebeat

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -12,19 +12,19 @@ def call() {
     }
     stage 'code quality test' {
         step {
-            sh run code quality test
+            sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host_url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+           // sh run code quality test
         }
     }
 
     stage 'checkmark sast' {
         step {
-             sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host_url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123'
-
+            sh 'sh run checkmark sast'
         }
     }
     stage 'checkmark sca' {
         step {
-
+       sh 'run checkmark sast'
         }
     }
 
