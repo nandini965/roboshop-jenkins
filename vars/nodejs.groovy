39ede9a23f026afc8518f7f026ce14316d32c9fb
nandini965 <nandhini965@gmail.com>
2023-12-14 17:50:46 +0530

    filebeat

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,7 +16,7 @@ def call() {
             stage('code quality') {
                 steps {
 
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true'
                     // sh run code quality test
                 }
             }
