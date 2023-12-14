73e42febc94d6d1ad8e3d3b6aa56967564276c08
nandini965 <nandhini965@gmail.com>
2023-12-14 19:10:20 +0530

    filebeat

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,4 +1,4 @@
-def call() {
+ def call() {
     pipeline {
 
         agent {
@@ -16,38 +16,38 @@ def call() {
                 sh 'maven compile'
             }
         }
-            stage('code quality') {
-                steps {
+        stage('code quality') {
+            steps {
 
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true -Dsonar.java.binaries=./target'
-                    // sh run code quality test
-                }
+                sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true -Dsonar.java.binaries=./target'
+                // sh run code quality test
             }
-
-            stage('Unit Test Cases') {
-                steps {
-                    sh 'echo Unit tests'
-                    //sh 'npm test'
-                }
-            }
-
-            stage('CheckMarx SAST Scan') {
-                steps {
-                    sh 'echo Checkmarx Scan'
-                }
-            }
-
-            stage('CheckMarx SCA Scan') {
-                steps {
-                    sh 'echo Checkmarx SCA Scan'
-                }
-            }
-
         }
-        post {
-            always {
-                cleanWs()
+
+        stage('Unit Test Cases') {
+            steps {
+                sh 'echo Unit tests'
+                //sh 'npm test'
             }
+        }
+
+        stage('CheckMarx SAST Scan') {
+            steps {
+                sh 'echo Checkmarx Scan'
+            }
+        }
+
+        stage('CheckMarx SCA Scan') {
+            steps {
+                sh 'echo Checkmarx SCA Scan'
+            }
+        }
+
+    }
+    post {
+        always {
+            cleanWs()
         }
     }
-}
+  }
+ }
