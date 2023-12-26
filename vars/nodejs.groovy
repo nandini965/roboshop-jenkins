c96cc1835afd19c9bacf45e3dfaf6f14b1819bc7
nandini965 <nandhini965@gmail.com>
2023-12-26 18:45:12 +0530

    filebeat

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,54 +1,58 @@
    def call() {
-    pipeline {
+       pipeline {
 
-        agent {
-            node {
-                label 'workstation'
-            }
-        }
+           agent {
+               node {
+                   label 'workstation'
+               }
+           }
 
-        options {
-            ansiColor('xterm')
-        }
+           options {
+               ansiColor('xterm')
+           }
 
-        stages {
-            stage('code quality') {
-                steps {
+           stages {
+               stage('code quality') {
+                   steps {
 
-                    //   sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true'
-                    sh 'echo code quality test'
-                }
-            }
+                       //   sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true'
+                       sh 'echo code quality test'
+                   }
+               }
 
-            stage('Unit Test Cases') {
-                steps {
-                    sh 'echo Unit tests'
-                    //sh 'npm test'
-                }
-            }
+               stage('Unit Test Cases') {
+                   steps {
+                       sh 'echo Unit tests'
+                       //sh 'npm test'
+                   }
+               }
 
-            stage('CheckMarx SAST Scan') {
-                steps {
-                    sh 'echo Checkmarx Scan'
-                }
-            }
-            stage('CheckMarx SCA Scan') {
-                steps {
-                    sh 'echo Checkmarx SCA Scan'
-                }
-            }
-            stage('Release Application') {
-                when {
-                    expression {
-                        env.TAG_NAME ==~ ".*"
-                    }
-                }
-            }
-        }
-        post {
-            always {
-                cleanWs()
-                }
-            }
-        }
-    }
+               stage('CheckMarx SAST Scan') {
+                   steps {
+                       sh 'echo Checkmarx Scan'
+                   }
+               }
+               stage('CheckMarx SCA Scan') {
+                   steps {
+                       sh 'echo Checkmarx SCA Scan'
+                   }
+               }
+               stage('Release Application') {
+                   steps {
+                       sh 'env'
+                       sh 'echo release application'
+                   }
+                   when {
+                       expression {
+                           env.TAG_NAME ==~ ".*"
+                       }
+                   }
+               }
+           }
+           post {
+               always {
+                   cleanWs()
+               }
+           }
+       }
+   }
