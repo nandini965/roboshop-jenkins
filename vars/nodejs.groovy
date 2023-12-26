9b2e611c908da01ea4891f21a1b2841e64704889
nandini965 <nandhini965@gmail.com>
2023-12-27 01:12:01 +0530

    filebeat

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -45,8 +45,9 @@
                        }
                    }
                    steps {
+                       sh 'npm install'
                        sh 'zip -r ${component}-${TAG_NAme}.zip node_modules server.js VERSION'
-                       sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/cart/server.js'
+                       sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}-${TAG_NAME}.zip'
                    }
                }
            }
