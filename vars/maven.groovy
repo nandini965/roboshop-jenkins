08ab66e69b9c118cd01e0e2638a47a5877c05bc6
nandini965 <nandhini965@gmail.com>
2024-01-03 01:48:35 +0530

    filebeat

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -20,44 +20,44 @@
                     sh 'mvn compile'
                 }
             }
-        stage('code quality') {
-            steps {
+            stage('code quality') {
+                steps {
 
-             //   sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true -Dsonar.java.binaries=./target'
-                 sh 'run code quality test'
+                    //   sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.32.12:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygates.wait=true -Dsonar.java.binaries=./target'
+                    sh 'run code quality test'
+                }
             }
-        }
 
-        stage('Unit Test Cases') {
-            steps {
-                sh 'echo Unit tests'
-                //sh 'npm test'
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                    //sh 'npm test'
+                }
             }
-        }
 
-        stage('CheckMarx SAST Scan') {
-            steps {
-                sh 'echo Checkmarx Scan'
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
             }
-        }
 
-        stage('CheckMarx SCA Scan') {
-            steps {
-                sh 'echo Checkmarx SCA Scan'
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
             }
-        }
             when {
                 expression {
                     env.TAG_NAME ==~ ".*"
                 }
-            }
+
             steps {
                 sh 'mvn package; cp target/${component}-1.0.jar ${component}.jar'
                 sh 'echo $TAG_NAME >VERSION'
                 sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
                 sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
-
+        }
     }
     post {
         always {
