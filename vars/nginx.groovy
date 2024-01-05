dbe3cfc8062f49164dee6b9ad73bb663e2f0da90
nandini965 <nandhini965@gmail.com>
2024-01-06 02:55:15 +0530

    filebeat

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -50,7 +50,7 @@ def call() {
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     sh 'zip -r ${component}-${TAG_NAME}.zip.Jenkinsfile'
-                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
         }
