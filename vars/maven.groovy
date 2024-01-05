64353b8ec7ca2849cf3539e61093e7751d86ecdb
nandini965 <nandhini965@gmail.com>
2024-01-06 02:06:51 +0530

    filebeat

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -55,8 +55,8 @@
             steps {
                 sh 'mvn package; cp target/${component}-1.0.jar ${component}.jar'
                 sh 'echo $TAG_NAME >VERSION'
-                sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
-                sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
+                sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.33.0:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
         }
     }
